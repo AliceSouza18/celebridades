a500817e7a8ade8e6c7ed6c7be1e8ede8a911e51
018liice <souzalicerj18@gmail.com>
2024-09-09 16:45:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Kim Dahyun",
+        descricao: "Kim Dahyun é uma cantora, rapper e dançarina sul-coreana, integrante do grupo feminino TWICE. Além de suas habilidades artísticas, ela é conhecida por sua personalidade divertida e carismática.",
+        link: "https://en.wikipedia.org/wiki/Dahyun",
+        tags:"dançarina rapper dubu"
+    },
+    {
+        titulo: "Kim Taehyung",
+        descricao: "Kim Taehyung, mais conhecido por seu nome artístico V, é um dos membros do grupo de K- pop mundialmente famoso BTS. Além de ser um vocalista e dançarino talentoso, V também se destaca como compositor e produtor musical.",
+        link: "https://en.wikipedia.org/wiki/V_%28singer%29",
+        tags:"ator tete V yeontan billboard Billboard solo"
+    },
+    {
+        titulo: "Hwasa",
+        descricao: "Hwasa é uma cantora sul-coreana, integrante do grupo feminino MAMAMOO. Conhecida por sua voz poderosa e performances carismáticas, ela também se destaca como compositora e modelo.im Dahyun é uma cantora, rapper e dançarina sul-coreana, integrante do grupo feminino TWICE. Além de suas habilidades artísticas, ela é conhecida por sua personalidade divertida e carismática.",
+        link: "https://en.wikipedia.org/wiki/Hwasa",
+        tags:"maria rapper solo Maria"
+    },
+];
+
